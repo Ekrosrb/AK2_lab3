5b881f03a183715cc50a2b6b498f226705f2c7a3
Ekrosrb <Ekrosrb@gmail.com>
2020-10-27 00:48:22 +0200

    AK2-Lab3 0.2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <getopt.h>
 using namespace std;
 
@@ -7,31 +8,32 @@ int v_flag = 0;
 int f_flag = 0;
 int s_flag = 0;
 int o_flag = 0;
-
+int l_flag = 0;
+int e_flag = 0;
 int a_flag = 1;
 
 int first = 0;
 int second = 0;
 char op;
 
+void list(char* args);
 void help();
 void version();
 void setFirst(char* value);
 void setSecond(char* value);
-void error(char c);
 
 
 int main(int args, char *argv[]) {
-//    setlocale(LC_ALL, NULL);
 
-    char sOpt[] = "hvf:s:o:";
+    char sOpt[] = "hvf:s:o:l:";
     static struct option lOpt[] = {
-            {"help", no_argument, NULL, 'h'},
-            {"version", no_argument, NULL, 'v'},
-            {"first", required_argument, NULL, 'f'},
-            {"second", required_argument, NULL, 's'},
-            {"operator", required_argument, NULL, 'o'},
-            {NULL, 0, NULL, 0}
+            {"help", no_argument, nullptr, 'h'},
+            {"version", no_argument, nullptr, 'v'},
+            {"first", required_argument, nullptr, 'f'},
+            {"second", required_argument, nullptr, 's'},
+            {"operator", required_argument, nullptr, 'o'},
+            {"list", required_argument, nullptr, 'l'},
+            {nullptr, 0, nullptr, 0}
 
     };
 
@@ -54,17 +56,26 @@ int main(int args, char *argv[]) {
                 setSecond(optarg);
                 break;
             case 'o':
-                if(o_flag != 0) continue;
+                if(o_flag) continue;
+                if(strlen(optarg) > 1){
+                    cout << "Parameters are incorrect: " << optarg << endl;
+                    return 0;
+                }
                 op = optarg[0];
                 o_flag = 1;
                 break;
+            case 'l':
+                list(optarg);
+                break;
             case '?':
             default:
-                error(c);
+                cout << "Invalid argument!" << c << endl;
+                e_flag = 1;
                 break;
         }
         longindex = -1;
     }
+    if(e_flag) return 0;
 
     if(f_flag && s_flag && o_flag){
         double ans;
@@ -102,9 +113,10 @@ int main(int args, char *argv[]) {
             cout << first << " " << op << " " << second << " = " << ans << endl;
         }else{
             cout << "A cant calculate your expression because:" << endl;
-            cout << "Unknown operation " << op;
+            cout << "Unknown operation " << op << endl;
+            return 0;
         }
-    }else if(f_flag || s_flag || o_flag){e
+    }else if(f_flag || s_flag || o_flag){
         cout << "A cant calculate your expression because:" << endl;
         if(!f_flag){
             cout << "Variable 'first' is not specified" << endl;
@@ -115,30 +127,70 @@ int main(int args, char *argv[]) {
         if(!o_flag){
             cout << "No operand specified" << endl;
         }
+        return 0;
 
     }
     return 0;
 }
 
+void list(char* args){
+    if(l_flag) return;
+    l_flag = 1;
+    int size = strlen(args);
+    int c = 0;
+    string buffer = "";
+    for(int i = 0; i < size; i++){
+        if(args[i] != ','){
+            buffer += args[i];
+        }else{
+            switch (c) {
+                case 0:
+                    first = atoi(buffer.c_str());
+                    f_flag = 1;
+                    break;
+                case 1:
+                    second = atoi(buffer.c_str());
+                    s_flag = 1;
+                    break;
+                default:
+                    cout << "Parameters are incorrect: " << optarg << endl;
+                    f_flag = 1;
+                    break;
+            }
+            c++;
+            buffer = "";
+        }
+    }
+    if(buffer.length() > 1 ){
+        cout << "Invalid operator: " << buffer << endl;
+        f_flag = 1;
+    }
+    o_flag = 1;
+    op = buffer.c_str()[0];
+
+}
 
 void help(){
     if(h_flag != 0) return;
-    cout << " This is help message to mini calculator: " << endl;
-    cout << " I can do something like: -f10 --second=5 -o+ out: 10 + 5 = 15" << endl;
-    cout << " I can perform operations such as: + - * /(without fractional part) | & % ^" << endl;
-    cout << " in:  -f10 --second=5 +" << endl;
-    cout << " out: 10 + 5 = 15" << endl << endl;
-    cout << " --help              -h         | to get this message" << endl;
-    cout << " --version           -v         | program version" << endl;
-    cout << " --first=[value]     -f[value]  | set first variable" << endl;
-    cout << " --second=[value]    -s[value]  | set second variable" << endl;
-    cout << " --operator=[value]  -o[value]  | set operator" << endl << endl;
+    cout << " This is help message to mini calculator: "                                    << endl;
+    cout << " I can do something like:"                                                     << endl;
+    cout << R"( in:  -f10 --second=5 -o"+"  or  -list=10,5,"+")"                            << endl;
+    cout << " out: 10 + 5 = 15"                                                     << endl << endl;
+    cout << R"( I can perform operations such as: + - * /(without fractional part) "|" "&" % ^)"<< endl;
+    cout << " For correct operation, operators & and | MUST be specified in double quotes!" << endl;
+    cout << " --help              -h            | to get this message"                      << endl;
+    cout << " --version           -v            | program version"                          << endl;
+    cout << " --first=<value>     -f<value>     | set first variable"                       << endl;
+    cout << " --second=<value>    -s<value>     | set second variable"                      << endl;
+    cout << " --operator=<value>  -o<value>     | set operator"                             << endl;
+    cout << " --list=<f>,<s>,<o>  -l<f>,<s>,<o> | set all values, but with this"            << endl;
+    cout << "                                     command, -f -s -o have no effect" << endl << endl;
     h_flag++;
 }
 
 void version(){
     if(v_flag != 0) return;
-    cout << "mini calculator version 0.1" << endl;
+    cout << "mini calculator version 0.2" << endl;
     v_flag++;
 }
 
@@ -152,8 +204,4 @@ void setSecond(char* value){
     if(s_flag != 0) return;
     second = atoi(value);
     s_flag++;
-}
-
-void error(char c){
-    cout << "Invalid argument " << c << endl;
 }

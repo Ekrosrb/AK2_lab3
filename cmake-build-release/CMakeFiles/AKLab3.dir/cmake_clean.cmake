41244b4b12d4a05bc2c090da488fa034b685ced4
Ekrosrb <Ekrosrb@gmail.com>
2020-10-27 00:55:40 +0200

    AK2-Lab3 full

---
+++ cmake-build-release/CMakeFiles/AKLab3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AKLab3"
+  "AKLab3.pdb"
+  "CMakeFiles/AKLab3.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AKLab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
